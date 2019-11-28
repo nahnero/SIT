8cdf878a98f686e20f27382c877bf3e84b5e842c
nah <nahnero@gmail.com>
2019-11-28 11:11:53 +0100

    caption links, table of contents, list of listings, list of figures, code for filter methods, wrapper methods, PCA and Pareto w/ figure

---
+++ latex/writeup.lol
@@ -0,0 +1,12 @@
+\contentsline {lstlisting}{\numberline {1}Importaciones iniciales y preparacion de datos.}{3}{lstlisting.1}% 
+\contentsline {lstlisting}{\numberline {2}C\'odigo generador de los histogramas con datos an\'omalos.}{4}{lstlisting.2}% 
+\contentsline {lstlisting}{\numberline {3}C\'odigo generador de los kernel density plots con datos an\'omalos.}{5}{lstlisting.3}% 
+\contentsline {lstlisting}{\numberline {4}C\'odigo generador de los boxplots con datos an\'omalos.}{6}{lstlisting.4}% 
+\contentsline {lstlisting}{\numberline {5}C\'odigo generador de los QQplots con datos an\'omalos.}{7}{lstlisting.5}% 
+\contentsline {lstlisting}{\numberline {6}C\'odigo generador de los corrplots con datos an\'omalos.}{8}{lstlisting.6}% 
+\contentsline {lstlisting}{\numberline {7}Aplicaci\'on m\'etodos \textit {filter} de selecci\'on caracter\'isticas.}{10}{lstlisting.7}% 
+\contentsline {lstlisting}{\numberline {8}Ranking de variables seg\'un los m\'etodos filter.}{10}{lstlisting.8}% 
+\contentsline {lstlisting}{\numberline {9}Aplicaci\'on m\'etodos \textit {wrapper} de selecci\'on caracter\'isticas.}{11}{lstlisting.9}% 
+\contentsline {lstlisting}{\numberline {10}Resultados del filtrado mediante wrappers.}{11}{lstlisting.10}% 
+\contentsline {lstlisting}{\numberline {11}\textit {Principal Component Analysis}}{12}{lstlisting.11}% 
+\contentsline {lstlisting}{\numberline {12}C\'odigo generador del diagrama de Pareto}{12}{lstlisting.12}% 
--- python/src/featureselection.py
+++ python/src/featureselection.py
@@ -11,22 +11,71 @@ according to different method
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-import ReliefF as rl
-import sklearn.feature_selection as sk
 from preprocessing import  x_no, y_no, labels
 
+#  Filter Methods
+import sklearn.feature_selection as sk
 
 Fscore, pval = sk.f_classif (x_no, y_no)
 r1 = Fscore.argsort().argsort() #  fscore rank
-
 print (r1+1)
+
+import ReliefF as rl
 
 r2 = rl.ReliefF (n_neighbors = 1) #  relieff rank
 r2.fit(x_no, y_no)
 r2 = r2.top_features
-
 print (r2+1)
 
 diferencias = abs (r1-r2)
+media = np.mean (diferencias)
+
 print (diferencias)
-print (np.mean (diferencias))
+print (media, end = '\n\n')
+
+#  Wrapper Methods
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsClassifier
+from mlxtend.feature_selection import SequentialFeatureSelector
+
+knn = KNeighborsClassifier (n_neighbors = 50)
+
+sfs = SequentialFeatureSelector (knn,
+                k_features = 4,
+                forward = True,
+                scoring = 'accuracy',
+                cv = 10)
+
+sfs.fit (x_no, y_no, custom_feature_names = labels)
+print (sfs.k_score_)
+print ('Sequential Forward  Selection', sfs.k_feature_names_, end = '\n\n')
+
+sfs.forward = False
+
+sfs.fit (x_no, y_no, custom_feature_names = labels)
+print (sfs.k_score_)
+print ('Sequential Backward Selection', sfs.k_feature_names_, end = '\n\n')
+
+#  PCA
+from sklearn.preprocessing import StandardScaler
+x_no = StandardScaler ().fit_transform (x_no) #  typify
+
+from sklearn.decomposition import PCA
+pca = PCA (n_components = 9)
+principalComponents = pca.fit_transform(x_no)
+evr = pca.explained_variance_ratio_
+
+print (evr)
+print (np.cumsum (evr))
+
+#  Pareto
+fig, ax = plt.subplots ()
+ax.bar (range (len (evr)), evr)
+ax.set_ylim (top=1)
+
+ax1 = ax.twinx ()
+ax1.set_ylim (top=100)
+ax1.plot (range (len (evr)), np.cumsum (evr)*100, marker = '.', color = 'red')
+
+fig.suptitle ('Pareto', fontsize = 20)
+fig.savefig  ('../images/pareto.pdf', bbox_inches = 'tight', pad_inches = 0)
